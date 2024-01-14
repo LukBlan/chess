3633982b9ceaf49eb5e5c0719496d8cecdbf4bb1
lukblan <luk.blanko@gmail.com>
2024-01-14 11:23:09 -0300

    Add simple move in game start

--- src/ui/console_interface.rb
+++ src/ui/console_interface.rb
@@ -6,17 +6,23 @@ class ConsoleInterface
   end
 
   def start
+    current_position = get_move_position
+    target_position = get_move_position
+    @board.move_piece(current_position, target_position)
+    @board_display.show_board(@board, @cursor)
+  end
+
+  private
+
+  def get_move_position
     loop do
       @board_display.show_board(@board, @cursor)
       result = @cursor.get_input
-
       system("clear")
 
       if result != nil
-        break
+        return result
       end
     end
-
-    @board_display.show_board(@board, @cursor)
   end
 end
